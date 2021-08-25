6d0906b9ed24d007a58517dcf89d4601f9e1a61a
erenada <erenada@gmail.com>
2021-08-25 11:55:17 -0400

    pick_contigs

---
+++ SISRS/scripts/pickContigs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#sorting the contigs by the number of SISRS sites they contain and copy to given directory
+
+
+# sort
+
+#cut -d'/' -f 1 alignment_locs_m5_nogap.txt | uniq -c | sort -bgr | sed 's/[[:space:]]//' > M_5_sorted_frequencies.csv
+
+# copy
+
+for contig in $(cut -d'/' -f 1 /data/schwartzlab/eren/Alignments/Ch_1_alignments/alignments_0821/alignment_locs_m5_nogap.txt | uniq -c | sort -bgr | sed 's/[[:space:]]*[[:digit:]]*[[:space:]]//' | head -50000);
+do
+  cp /data/schwartzlab/eren/Chapter1/CONTIGS/contigs_outputs/$contig.fasta /data/schwartzlab/eren/Chapter1/SISRS_Run/contigs_outputs
+done
