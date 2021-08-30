ae337c97b90106744565c7d15e13876733cb4301
erenada <erenada@gmail.com>
2021-08-30 15:39:19 -0400

    fix link

--- CONTIGS/Annotation/separateContigs.sh
+++ CONTIGS/Annotation/separateContigs.sh
@@ -5,5 +5,5 @@ listOfData=$(cat annot.csv | tail -n +2 | cut -f 2 | sort | uniq)
 for type in $listOfData;
 do
   mkdir $type
-  ln -s /data/schwartzlab/eren/Chapter1/CONTIGS/contigs_5_missing/$(tail -n +2 "$type"_table.csv | cut -f 1) /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/$type
+  ln -s /data/schwartzlab/eren/Chapter1/CONTIGS/contigs_5_missing/$(tail -n +2 "$type"_table.csv | cut -f 1) /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/$type/$(tail -n +2 "$type"_table.csv | cut -f 1)
 done
