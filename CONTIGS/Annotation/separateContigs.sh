bc07f03fbfadbe97d26bbb24a233b5e6ea3aedd7
erenada <erenada@gmail.com>
2021-08-30 15:32:46 -0400

    functionmerge

--- CONTIGS/Annotation/separateContigs.sh
+++ CONTIGS/Annotation/separateContigs.sh
@@ -5,10 +5,5 @@ listOfData=$(cat annot.csv | tail -n +2 | cut -f 2 | sort | uniq)
 for type in $listOfData;
 do
   mkdir $type
-  done
-
-
-for folder in $listOfData;
-do
-   ln -s /data/schwartzlab/eren/Chapter1/CONTIGS/contigs_5_missing/$(tail -n +2 "$folder"_table.csv | cut -f 1) /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/$folder/
+  ln -s /data/schwartzlab/eren/Chapter1/CONTIGS/contigs_5_missing/$(tail -n +2 "$type"_table.csv | cut -f 1) /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/$type
 done
