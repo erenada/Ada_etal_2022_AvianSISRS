e0ec00fb48f3c469e23ecb08769dfc91b431c8ea
erenada <erenada@gmail.com>
2021-08-30 15:07:31 -0400

    test

--- CONTIGS/Annotation/separateContigs.sh
+++ CONTIGS/Annotation/separateContigs.sh
@@ -10,6 +10,6 @@ do
 
 for folder in $listOfData;
 do
-  contigName=$(tail -n +2 "$file"_table.csv | cut -f 1)
+  contigName=(tail -n +2 "$file"_table.csv | cut -f 1)
    ln -s /data/schwartzlab/eren/Chapter1/CONTIGS/contigs_5_missing/$contigName /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/$folder
 done
