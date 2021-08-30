7724a4e6b140f8baf1f0433c1dc4f710fc61c18b
erenada <erenada@gmail.com>
2021-08-30 14:50:50 -0400

    fix

--- CONTIGS/Annotation/separateContigs.sh
+++ CONTIGS/Annotation/separateContigs.sh
@@ -8,7 +8,7 @@ do
   done
 
 
-for file in $(ls "listOfData"_table.csv);
+for file in $(ls "$listOfData"_table.csv);
 do
   contigName=$(tail -n + 2 $file | cut -f 1)
    ln -s /data/schwartzlab/eren/Chapter1/CONTIGS/contigs_5_missing/$contigName /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/$listOfData
