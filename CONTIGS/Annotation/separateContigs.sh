6c3555012c86a5b4e8da3539748e7a9e1928c76d
erenada <erenada@gmail.com>
2021-08-30 14:49:53 -0400

    sepcontigs

--- CONTIGS/Annotation/separateContigs.sh
+++ CONTIGS/Annotation/separateContigs.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-listOfData=$(cat annot_table.csv | cut -f 2 | sort | uniq)
+listOfData=$(tail -n +2 annot_table.csv | cut -f 2 | sort | uniq)
 
 for type in $listOfData;
 do
   mkdir $type
-  contigName=$(cut -f 1 "$type"_table.csv)
-  cd $type
-  cp /data/schwartzlab/eren/Chapter1/CONTIGS/contigs_5_missing/$contigName ./$type
+  done
+
+
+for file in $(ls "listOfData"_table.csv);
+do
+  contigName=$(tail -n + 2 $file | cut -f 1)
+   ln -s /data/schwartzlab/eren/Chapter1/CONTIGS/contigs_5_missing/$contigName /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/$listOfData
 done
