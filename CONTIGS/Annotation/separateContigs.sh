fb04dfa3f15cb25d0d453cb3f1306d1b059f2c33
erenada <erenada@gmail.com>
2021-08-31 10:11:43 -0400

    linking-contigs

--- CONTIGS/Annotation/separateContigs.sh
+++ CONTIGS/Annotation/separateContigs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+
 listOfData=$(cat annot.csv | tail -n +2 | cut -f 2 | sort | uniq)
 
 for type in $listOfData;
 do
+  alignmentName=$(tail -n +2 /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/"$type"_table.csv | cut -f 1)
   mkdir $type
-  ln -s /data/schwartzlab/eren/Chapter1/CONTIGS/contigs_5_missing/$(tail -n +2 "$type"_table.csv | cut -f 1) /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/$type/$(tail -n +2 "$type"_table.csv | cut -f 1)
+  ln -s /data/schwartzlab/eren/Chapter1/CONTIGS/contigs_5_missing/$alignmentName /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/$type/$alignmentName
 done
