973a1e9954e1db600a6c909a889620816f62cf5a
erenada <erenada@gmail.com>
2021-08-31 13:48:44 -0400

    sepcontigs

--- CONTIGS/Annotation/separateContigs.sh
+++ CONTIGS/Annotation/separateContigs.sh
@@ -7,7 +7,10 @@ listOfData=$(cat annot.csv | tail -n +2 | cut -f 2 | sort | uniq)
 for type in $listOfData;
 do
   mkdir $type
-  declare -a alignmentList
-  alignmentList=(tail -n +2 /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/"$type"_table.csv | cut -f 1)
-  ln -s /data/schwartzlab/eren/Chapter1/SISRS_Run/contigs_outputs/${alignmentList[*]} /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/$type/
+
+  alignmentList=$(tail -n +2 /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/"$type"_table.csv | cut -f 1)
+  for file in $alignmentList;
+  do
+    ln -s /data/schwartzlab/eren/Chapter1/CONTIGS/contigs_5_missing/${file} /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/${type}/${file}
+  done
 done
