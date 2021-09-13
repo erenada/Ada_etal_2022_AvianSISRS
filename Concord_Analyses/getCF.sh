e786c170ba2f778baa8a3817ff8737fd6732602c
erenada <erenada@gmail.com>
2021-09-13 19:51:33 -0400

    iqtree-mpi

--- Concord_Analyses/getCF.sh
+++ Concord_Analyses/getCF.sh
@@ -17,17 +17,17 @@ module purge
 
 module load IQ-TREE/2.1.2-foss-2020a
 
-ALN_DIR=$(/data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/ALIGNED)
+ALN_DIR=/data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/ALIGNED
 
-REF_TREE=$(/data/schwartzlab/eren/Chapter1/Concord_Analyses/ReferenceTrees)
+REF_TREE=/data/schwartzlab/eren/Chapter1/Concord_Analyses/ReferenceTrees
 
 cd $ALN_DIR
 
 for type in $(ls $ALN_DIR);
 do
-  iqtree -S ${ALN_DIR}/${type} --prefix "$type"_loci -T AUTO
+  mpirun -np 10 iqtree -mpi -nt 2 -S ${ALN_DIR}/${type} --prefix "$type"_loci -T AUTO
   for tree in $(ls $REF_TREE);
   do
-    iqtree -t ${REF_TREE}/${tree} --gcf ${ALN_DIR}/${type}/"$type"_loci -p ${ALN_DIR}/${type} --scf 100 --prefix "$type"concord -T 20
+    mpirun -np 4 iqtree -mpi -nt 2 -t ${REF_TREE}/${tree} --gcf ${ALN_DIR}/${type}/"$type"_loci -p ${ALN_DIR}/${type} --scf 100 --prefix "$type"concord -T 20
   done
 done
