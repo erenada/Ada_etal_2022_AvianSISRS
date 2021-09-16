05cc9eefcbb0de838221e7df7bc9f334dc89cea3
Eren Ada <erenada@gmail.com>
2021-09-16 18:26:58 -0400

    locusRun.sh

---
+++ Concord_Analyses/other_locusRun.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name="concordValues"
+#SBATCH --time=170:00:00  # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=20   # CHANGE THIS to processor core(s) per node
+#SBATCH --mail-user="erenada@uri.edu" #CHANGE THIS to your user email address
+#SBATCH --mail-type=END,FAIL
+#SBATCH -o %x_%A.out
+#SBATCH -e %x_%A.err
+#SBATCH --exclusive
+
+#get started here
+
+## Calculating gene/locus trees
+
+module purge
+
+module load IQ-TREE/2.1.2-foss-2020a
+
+ALN_DIR=/data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/ALIGNED
+
+REF_TREE=/data/schwartzlab/eren/Chapter1/Concord_Analyses/ReferenceTrees
+
+cd $ALN_DIR
+iqtree2 -S /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/ALIGNED/other/ --prefix other_loci -nt 24
