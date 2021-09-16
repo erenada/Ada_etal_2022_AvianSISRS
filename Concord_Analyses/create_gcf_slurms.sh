62d7813ebbbbbdf67bb1e2ab67afc8caad63180f
erenada <erenada@gmail.com>
2021-09-16 17:57:30 -0400

    tidier

--- Concord_Analyses/create_gcf_slurms.sh
+++ Concord_Analyses/create_gcf_slurms.sh
@@ -10,9 +10,11 @@ cd $ALN_DIR
 
 for type in $(ls -d */ | sed -e "s/\///g");
 do
+  cp data/schwartzlab/eren/Chapter1/Concord_Analyses/Template.sh
   echo "iqtree2 -S ${ALN_DIR}/${type}/ --prefix "${type}"_loci -nt 24" > "${type}"_locusRun.sh
   cat Template.sh "${type}"_locusRun.sh > "${type}"_locusRun.sh
   mv "${type}"_locusRun.sh /data/schwartzlab/eren/Chapter1/Concord_Analyses/"${type}"_locusRun.sh
+  rm Template.sh
   cd /data/schwartzlab/eren/Chapter1/Concord_Analyses/
   sbatch "${type}"_locusRun.sh
 done
