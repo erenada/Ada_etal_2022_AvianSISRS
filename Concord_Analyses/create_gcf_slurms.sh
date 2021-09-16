27d36009146a35281e0e156ded1cc9507681a41a
erenada <erenada@gmail.com>
2021-09-16 18:05:26 -0400

    ready

--- Concord_Analyses/create_gcf_slurms.sh
+++ Concord_Analyses/create_gcf_slurms.sh
@@ -6,15 +6,22 @@ ALN_DIR=/data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/ALIGNED
 REF_TREE=/data/schwartzlab/eren/Chapter1/Concord_Analyses/ReferenceTrees
 
 
+cp data/schwartzlab/eren/Chapter1/Concord_Analyses/Template.sh ${ALN_DIR}/Template.sh
+
 cd $ALN_DIR
 
 for type in $(ls -d */ | sed -e "s/\///g");
 do
-  cp data/schwartzlab/eren/Chapter1/Concord_Analyses/Template.sh ./
-  echo "iqtree2 -S ${ALN_DIR}/${type}/ --prefix "${type}"_loci -nt 24" > "${type}"_locusRun.sh
-  cat Template.sh "${type}"_locusRun.sh > "${type}"_locusRun.sh
+  echo "iqtree2 -S ${ALN_DIR}/${type}/ --prefix "${type}"_loci -nt 24" > "${type}"_temp.sh
+  cat Template.sh "${type}"_temp.sh > "${type}"_locusRun.sh
   mv "${type}"_locusRun.sh /data/schwartzlab/eren/Chapter1/Concord_Analyses/"${type}"_locusRun.sh
-  rm Template.sh
-  cd /data/schwartzlab/eren/Chapter1/Concord_Analyses/
-  sbatch "${type}"_locusRun.sh
+done
+
+rm *_temp.sh
+rm Template.sh
+
+cd /data/schwartzlab/eren/Chapter1/Concord_Analyses/
+
+sbatch "${type}"_locusRun.sh
+
 done
