84957d30ae63d48987eb818ab65a0f8af5f56134
erenada <erenada@gmail.com>
2021-09-16 17:52:47 -0400

    create_gcf_slurm

---
+++ Concord_Analyses/create_gcf_slurms.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+ALN_DIR=/data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/ALIGNED
+
+REF_TREE=/data/schwartzlab/eren/Chapter1/Concord_Analyses/ReferenceTrees
+
+
+cd $ALN_DIR
+
+for type in $(ls -d */ | sed -e "s/\///g");
+do
+  echo "iqtree2 -S ${ALN_DIR}/${type}/ --prefix "${type}"_loci -nt 24" > "${type}"_locusRun.sh
+  cat Template.sh "${type}"_locusRun.sh > "${type}"_locusRun.sh
+  mv "${type}"_locusRun.sh /data/schwartzlab/eren/Chapter1/Concord_Analyses/"${type}"_locusRun.sh
+  cd /data/schwartzlab/eren/Chapter1/Concord_Analyses/
+  sbatch "${type}"_locusRun.sh
+done
