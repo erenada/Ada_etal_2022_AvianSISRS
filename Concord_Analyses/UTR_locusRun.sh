416efb245e6c88b980908123a0633a7cd4b26cc7
erenada <erenada@gmail.com>
2021-09-17 14:45:54 -0400

    nt1

--- Concord_Analyses/UTR_locusRun.sh
+++ Concord_Analyses/UTR_locusRun.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name="concordValues"
 #SBATCH --time=170:00:00  # walltime limit (HH:MM:SS)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --ntasks-per-node=24   # CHANGE THIS to processor core(s) per node
+#SBATCH --ntasks-per-node=$SLURM_NTASKS_PER_NODE   # CHANGE THIS to processor core(s) per node
 #SBATCH --mail-user="erenada@uri.edu" #CHANGE THIS to your user email address
 #SBATCH --mail-type=END,FAIL
 #SBATCH -o UTR_loci_%A.out
@@ -27,8 +27,6 @@ module load OpenMPI/4.0.3-GCC-9.3.0
 
 module load IQ-TREE/2.1.2-foss-2020a
 
-$SLURM_NTASKS_PER_NODE
-
 
 ALN_DIR=/data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/ALIGNED
 
@@ -36,4 +34,4 @@ REF_TREE=/data/schwartzlab/eren/Chapter1/Concord_Analyses/ReferenceTrees
 
 cd $ALN_DIR
 
-mpirun -np 1 iqtree2-mpi -nt 24 -S /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/ALIGNED/UTR/ --prefix UTR_loci -mem 64G
+mpirun -np 1 iqtree2-mpi -nt 1 -S /data/schwartzlab/eren/Chapter1/CONTIGS/Annotation/ALIGNED/UTR/ --prefix UTR_loci -mem 64G
