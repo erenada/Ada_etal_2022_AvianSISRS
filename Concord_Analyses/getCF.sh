565c8a922786b83d0f8308d8e6409c7c28df77d3
erenada <erenada@gmail.com>
2021-09-18 07:36:47 -0400

    treetype

--- Concord_Analyses/getCF.sh
+++ Concord_Analyses/getCF.sh
@@ -33,6 +33,6 @@ for tree in $(ls $REF_TREE);
 do
   for type in $(ls -d */ | sed -e "s/\///g");
   do
-    /data/schwartzlab/eren/programs/iqtree-2.1.3-Linux/bin/iqtree2 -t ${REF_TREE}/${tree} --gcf ${ALN_DIR}/${type}_loci.treefile -s ${ALN_DIR}/${type}/${type}_concat.fasta --scf 100 --prefix concord -nt 20
+    /data/schwartzlab/eren/programs/iqtree-2.1.3-Linux/bin/iqtree2 -t ${REF_TREE}/${tree} --gcf ${ALN_DIR}/${type}_loci.treefile -s ${ALN_DIR}/${type}/${type}_concat.fasta --scf 100 --prefix ${tree}_${type}_concord -nt 20
 done
 done
